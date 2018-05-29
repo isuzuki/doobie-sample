78ae4e2b16bda216c978c08478d344861fe7b04a
isuzuki <ikkuntech@gmail.com>
2018-05-30 01:33:10 +0900

    modifled database values

--- src/test/scala/io/github/isuzuki/ConfigTest.scala
+++ src/test/scala/io/github/isuzuki/ConfigTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.{Matchers, WordSpec}
 class ConfigTest extends WordSpec with Matchers {
   "loadConfig" should {
     "return database values" in {
-      config.database.user shouldBe "test-user"
-      config.database.pass shouldBe "test-pass"
+      config.database.user shouldBe "test_user"
+      config.database.pass shouldBe "test_pass"
       config.database.host shouldBe "127.0.0.1"
       config.database.db   shouldBe "test_db"
     }
