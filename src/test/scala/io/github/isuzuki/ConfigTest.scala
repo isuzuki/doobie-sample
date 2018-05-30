a87400e13ebf61dec50fa50c50f845476ae83c67
isuzuki <ikkuntech@gmail.com>
2018-05-31 02:07:04 +0900

    added mysql settings for ci

--- src/test/scala/io/github/isuzuki/ConfigTest.scala
+++ src/test/scala/io/github/isuzuki/ConfigTest.scala
@@ -5,10 +5,8 @@ import org.scalatest.{Matchers, WordSpec}
 class ConfigTest extends WordSpec with Matchers {
   "loadConfig" should {
     "return database values" in {
-      config.database.user shouldBe "doobie_user"
-      config.database.pass shouldBe "doobie_pass"
-      config.database.host shouldBe "192.168.99.100"
-      config.database.db   shouldBe "doobie_sample"
+      config.database.isInstanceOf[Database] shouldBe true
+      config.database.db                     shouldBe "doobie_sample"
     }
   }
 }
